ce2cf918f9466c5c7d223ab81ce49af4cf74ccf6
Rowan Knight <46970628+mountinash789@users.noreply.github.com>
2021-12-06 22:40:23 +0000

    answers

--- Day1/challenge.py
+++ Day1/challenge.py
@@ -1,6 +1,20 @@
 def part_one(lst):
-    pass
-
+    c = 0
+    pre = None
+    for x in lst:
+        x = int(x)
+        if pre:
+            if x > pre:
+                c +=1
+    return c
 
 def part_two(lst):
-    pass
+    c = 0
+    pre = None
+    for i, x in enumerate(lst):
+        if i+2 < len(lst):
+            summ = sum([int(lst[i]),int(lst[i+1]),int(lst[i+2])])
+            if pre:
+                if summ > pre:
+                   c+=1
+    return c
