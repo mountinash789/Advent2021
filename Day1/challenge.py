b932c630a672d937edaa71117cbbc011f4d39b5b
Rowan Knight <mountinash@gmail.com>
2021-12-06 22:58:31 +0000

    Update challenge.py

--- Day1/challenge.py
+++ Day1/challenge.py
@@ -5,16 +5,26 @@ def part_one(lst):
         x = int(x)
         if pre:
             if x > pre:
-                c +=1
+                c += 1
+        pre = x
     return c
+
 
 def part_two(lst):
     c = 0
     pre = None
     for i, x in enumerate(lst):
-        if i+2 < len(lst):
-            summ = sum([int(lst[i]),int(lst[i+1]),int(lst[i+2])])
+        if i + 2 < len(lst):
+            sum_ = sum([int(lst[i]), int(lst[i + 1]), int(lst[i + 2])])
             if pre:
-                if summ > pre:
-                   c+=1
+                if sum_ > pre:
+                    c += 1
+            pre = sum_
     return c
+
+
+if __name__ == "__main__":
+    with open('input.txt') as file:
+        lst = file.read().split()
+        print(part_one(lst))
+        print(part_two(lst))
