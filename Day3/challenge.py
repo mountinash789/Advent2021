d1f39081bbc6c4cb08dc6a70abec18efff7dd5ab
Rowan Knight <mountinash@gmail.com>
2021-12-07 00:17:58 +0000

    part 2 - ugly code

--- Day3/challenge.py
+++ Day3/challenge.py
@@ -14,7 +14,7 @@ def part_one(lst):
     one = occurrence_map(lst, '1')
     zero = occurrence_map(lst, '0')
 
-    for x in range(12):
+    for x in range(len(lst[0])):
         if one[x] > zero[x]:
             gamma.append('1')
             epsilon.append('0')
@@ -25,8 +25,33 @@ def part_one(lst):
     return int(''.join(gamma), 2) * int(''.join(epsilon), 2)
 
 
+def myfilter(lst, char, pos, match):
+    if len(lst) == 1:
+        return lst.pop()
+
+    one = occurrence_map(lst, '1')
+    zero = occurrence_map(lst, '0')
+    if one[pos] > zero[pos]:
+        if char == '1':
+            match.append('1')
+        else:
+            match.append('0')
+    elif one[pos] < zero[pos]:
+        if char == '1':
+            match.append('0')
+        else:
+            match.append('1')
+    else:
+        match.append(char)
+    lst = list(filter(lambda x: x[:pos + 1] == match, lst))
+
+    return myfilter(lst, char, pos + 1, match)
+
+
 def part_two(lst):
-    pass
+    oxygen = myfilter(lst, '1', 0, [])
+    co2 = myfilter(lst, '0', 0, [])
+    return int(''.join(oxygen), 2) * int(''.join(co2), 2)
 
 
 if __name__ == "__main__":
@@ -34,5 +59,5 @@ if __name__ == "__main__":
         data = []
         for line in file.read().split():
             data.append(list(line))
-        print(part_one(data))
-        # print(part_two(data))
+        # print(part_one(data))
+        print(part_two(data))
